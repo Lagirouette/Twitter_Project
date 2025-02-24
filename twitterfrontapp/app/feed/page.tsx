7dc74a76614f67a6cd7c79dfad2fd656b7f18afd
andre moreira <andrefurtado122002@gmail.com>
2025-02-25 00:46:47 +0100

    Connection to api

--- twitterfrontapp/app/feed/page.tsx
+++ twitterfrontapp/app/feed/page.tsx
@@ -8,16 +8,15 @@ import LeftSideBar from "@/components/_lsidebar/lsidebar";
 
 
 type Post = {
-    userId: number;
-    id: number;
+    createdBy: string;
+    createOn: Date;
     title: string;
     body: string;
 }
 
 export default async function PostFeed() {
-    const response = await fetch("https://jsonplaceholder.typicode.com/posts")
+    const response = await fetch("http://localhost:5130/api/post")
     const posts: Post[] = await response.json()
-    const filteredPosts = posts.filter((post) => post.id % 10 === 1);
     console.log(posts)
 
     return (
@@ -34,20 +33,21 @@ export default async function PostFeed() {
                 <div className="shadow-md p-5 border border-b-8 border-solid border-gray-700">
                     <PostTextArea/>
                 </div>
-                {filteredPosts.map((post) => (
+                {posts.map((post) => (
                     <div className="flex shadow-md p-4 border border-solid border-gray-700">
                         <div className="px-2">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6 h-8 w-8 flex-none">
                                 <path d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
                             </svg>
                         </div>
-                        <div key={post.id} className="">
+                        <div className="w-full">
                             <Suspense
                             fallback={
                                 <div className="text-sm text-white-500">Loading author...</div>
                             }
                             >
-                            <Author userId={post.userId} />
+                            <p>{post.createdBy} <span className="text-sm text-gray-500"> @{post.createdBy}</span></p>    
+                            {/* <Author userId={post.userId} /> */}
                             </Suspense>
                             <p className="text-white-600 mb-4 leading-relaxed">{post.body}</p>
                             
